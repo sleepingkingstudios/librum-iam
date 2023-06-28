db92a074b412c290833efe03b944197938b7d8a9
Rob Smith <merlin@sleepingkingstudios.com>
2023-06-28 19:11:23 -0400

    Implement View::SessionsController.

---
+++ app/controllers/librum/iam/view/sessions_controller.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+require 'librum/core/responders/html/view_responder'
+require 'librum/core/view/layouts/page'
+require 'librum/iam/actions/sessions/create'
+require 'librum/iam/actions/sessions/destroy'
+require 'librum/iam/authentication/errors/invalid_login'
+
+module Librum::Iam::View
+  # Controller for managing authentication sessions.
+  class SessionsController < Librum::Core::ViewController
+    # Responder class for authentication responses.
+    class Responder < Librum::Core::Responders::Html::ViewResponder
+      action :create do
+        match :success do
+          redirect_back
+        end
+
+        match :failure,
+          error: Librum::Iam::Authentication::Errors::InvalidLogin \
+        do
+          alert = {
+            icon:    'user-xmark',
+            message: 'Invalid username or password.'
+          }
+
+          redirect_back(flash: { danger: alert })
+        end
+      end
+
+      action :destroy do
+        match :success do
+          alert = {
+            icon:    'user-xmark',
+            message: 'You have successfully logged out.'
+          }
+
+          redirect_to(
+            '/',
+            flash: { warning: alert }
+          )
+        end
+      end
+    end
+
+    def self.resource
+      Librum::Core::Resources::BaseResource.new(
+        resource_name:       'sessions',
+        skip_authentication: %i[create destroy]
+      )
+    end
+
+    responder :html, Librum::Iam::View::SessionsController::Responder
+
+    action :create,  Librum::Iam::Actions::Sessions::Create
+
+    action :destroy, Librum::Iam::Actions::Sessions::Destroy
+  end
+end
---
+++ lib/librum/iam/actions/sessions.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'librum/iam/actions'
+
+module Librum::Iam::Actions
+  # Namespace for Librum::Iam::SessionsController actions.
+  module Sessions; end
+end
