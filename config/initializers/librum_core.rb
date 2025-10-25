378cf9946a5217434a6bddec63e1712c91d0b136
Rob Smith <sleepingkingstudios@gmail.com>
2025-10-25 19:22:34 -0400

    Fix SessionsController.resource class.

--- config/initializers/librum_core.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-Librum::Core::Engine.instance_exec do
-  config.after_initialize do
-    # Add authentication config to all controller resources.
-    Librum::Core::Resources::BaseResource.include(Librum::Iam::Resource)
-  end
-end
