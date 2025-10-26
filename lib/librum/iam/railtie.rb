d14b70af4fb46f8f8707077e2ad53772d22e54c8
Rob Smith <sleepingkingstudios@gmail.com>
2025-10-25 20:40:46 -0400

    Fix autoloading extensions.

--- lib/librum/iam/railtie.rb
+++ lib/librum/iam/railtie.rb
@@ -4,5 +4,18 @@ require 'librum/iam'
 
 module Librum::Iam
   # Handles Rails configuration and initializers.
-  class Railtie < Rails::Railtie; end
+  class Railtie < Rails::Railtie
+    root_path = Pathname.new(__dir__).join('../../..')
+    overrides = root_path.join('lib/ext')
+
+    config.before_configuration do
+      Rails.autoloaders.each { |autoloader| autoloader.ignore(overrides) }
+    end
+
+    config.to_prepare do
+      Dir.glob("#{overrides}/**/*.rb").each do |override|
+        load override
+      end
+    end
+  end
 end
